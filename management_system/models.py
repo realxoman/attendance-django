1b319f5f6d24964c0eef936a3ad785c8f5f50511
alixo <fromiran98@gmail.com>
2021-03-08 16:18:21 +0330

    0.0.4

--- management_system/models.py
+++ management_system/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.fields import DateTimeField
 
 
 # Create your models here.
@@ -16,6 +17,8 @@ class TarahiClass(models.Model):
 class UserTerm(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, null=True, blank=True,on_delete=models.CASCADE,verbose_name="نام کاربری هنرجو")
+    date_pay = models.DateField(auto_now=False, auto_now_add=False,null=True, blank=True,verbose_name="تاریخ پرداخت")
+    date_first = models.DateTimeField(auto_now=False, auto_now_add=False,null=True, blank=True,verbose_name="زمان اولین جلسه")
     def __str__(self):
         return self.name
     class Meta:
