7a437b11beca07bbeca17c635cec29b2ac8b9956
alixo <fromiran98@gmail.com>
2021-03-09 15:41:49 +0330

    0.0.5

--- management_system/admin.py
+++ management_system/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import TarahiClass,UserTerm
+from .models import TarahiClass,UserTerm,Jalase
 
 # Register your models here.
+class Jalase_Inline(admin.TabularInline):
+    model = Jalase
+    extra = 8
+
+
+class UserTermAdmin(admin.ModelAdmin):
+    list_display = ('classname','user')
+    inlines = [
+        Jalase_Inline,
+    ]
 
 admin.site.register(TarahiClass)
-admin.site.register(UserTerm)
+
+admin.site.register(UserTerm,UserTermAdmin)
+
+admin.site.register(Jalase)
--- management_system/models.py
+++ management_system/models.py
@@ -7,20 +7,32 @@ from django.db.models.fields import DateTimeField
 
 class TarahiClass(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50,default="ساده",verbose_name="نام")
-    level = models.CharField(max_length=50,default="ساده",verbose_name="سطح")
+    name = models.CharField(max_length=50,default="ساده", verbose_name="نام")
+    level = models.CharField(max_length=50,default="ساده", verbose_name="سطح")
     class Meta:
         verbose_name = 'نوع کلاس'
         verbose_name_plural = 'انواع کلاس ها'
+    def __str__(self):
+        return self.name
 
 
 class UserTerm(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, null=True, blank=True,on_delete=models.CASCADE,verbose_name="نام کاربری هنرجو")
-    date_pay = models.DateField(auto_now=False, auto_now_add=False,null=True, blank=True,verbose_name="تاریخ پرداخت")
-    date_first = models.DateTimeField(auto_now=False, auto_now_add=False,null=True, blank=True,verbose_name="زمان اولین جلسه")
+    classname = models.ForeignKey(TarahiClass, null=True, blank=True, on_delete=models.CASCADE, verbose_name="انتخاب کلاس")
+    user = models.ForeignKey(User, null=True, blank=True,on_delete=models.CASCADE, verbose_name="نام کاربری هنرجو")
+    date_pay = models.DateField(auto_now=False, auto_now_add=False,null=True, blank=True, verbose_name="تاریخ پرداخت")
     def __str__(self):
-        return self.name
+        return self.user.get_full_name() + " " + self.classname.name
     class Meta:
         verbose_name = 'کلاس کاربران'
         verbose_name_plural = 'کلاس های کاربران'
+
+class Jalase(models.Model):
+    attend = models.BooleanField(null=True, verbose_name="حضور")
+    date_jalase = models.DateTimeField(null=True, verbose_name="زمان جلسه")
+    userterm =  models.ForeignKey(UserTerm, null=True, blank=True, on_delete=models.CASCADE, verbose_name="انتخاب کلاس", related_name="jalasat")
+    class Meta:
+        verbose_name = 'جلسات'
+        verbose_name_plural = 'جلسه'
+
+    
